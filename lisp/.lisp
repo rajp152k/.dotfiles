84005b04a40e52c74537505bc15782db31e1adac
(Bit-Mage) <raj@thebitmage.com>
2024-11-15 17:11:43 +0530

    lisp
    Signed-off-by: (Bit-Mage) <raj@thebitmage.com>

--- lisp/.lisp
+++ lisp/.lisp
@@ -185,3 +185,20 @@
       (if (funcall quit in)
           (return)
           (format *query-io* "~A~%" (funcall fn in))))))
+
+
+(defun mkstr (&rest args)
+  (with-output-to-string (s)
+    (dolist  (a args) (princ a s))))
+
+(defun symb (&rest args)
+  (values (read-from-string (apply #'mkstr args))))
+
+(defun reread (&rest args)
+  (values (read-from-string (apply #'mkstr args))))
+
+(defun explode (sym)
+  (map 'list #'(lambda (c)
+                 (intern (make-string 1
+                                      :initial-element c)))
+       (symbol-name sym)))
