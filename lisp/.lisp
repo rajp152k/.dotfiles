546c9c61ad2377f8b63da445f3f8c444d3db7a89
(Bit-Mage) <raj@thebitmage.com>
2024-11-15 18:44:57 +0530

    lisp
    Signed-off-by: (Bit-Mage) <raj@thebitmage.com>

--- lisp/.lisp
+++ lisp/.lisp
@@ -202,3 +202,81 @@
                  (intern (make-string 1
                                       :initial-element c)))
        (symbol-name sym)))
+
+(defun memoize (fn)
+  (let ((cache (make-hash-table :test #'equal)))
+    #'(lambda (&rest args)
+        (multiple-value-bind (val hit) (gethash args cache)
+          (if hit
+              val
+              (setf (gethash args cache)
+                    (apply fn args)))))))
+
+
+(defun compose (&rest fns)
+  (if fns
+      (let ((fn1 (last1 fns))
+            (fns (butlast fns)))
+        #'(lambda (&rest args)
+            (reduce #'funcall fns
+                    :from-end t
+                    :initial-value (apply fn1 args))))
+      #'identity))
+
+(defun fif (if then &optional else)
+  #'(lambda (x)
+      (if (funcall if x)
+          (funcall then x)
+          (if else (funcall else x)))))
+
+(defun fint (fn &rest fns)
+  (if (null fns)
+      fn
+      (let ((chain (apply #'fint fns)))
+        #'(lambda (x)
+            (and (funcall fn x) (funcall chain x))))))
+
+(defun fun (fn &rest fns)
+  (if (null fns)
+      fn
+      (let ((chain (apply #'fun fns)))
+        #'(lambda (x)
+            (or (funcall fn x) (funcall chain x))))))
+
+(defun lrec (rec &optional base)
+  (labels ((self (lst)
+             (if (null lst)
+                 (if (functionp base)
+                     (funcall base)
+                     base)
+                 (funcall rec (car lst)
+                          #'(lambda ()
+                              (self (cdr lst)))))))
+    #'self))
+
+(defun ttrav (rec &optional (base #'identity))
+  (labels ((self (tree)
+             (if (atom tree)
+                 (if (functionp base)
+                     (funcall base tree)
+                     base)
+                 (funcall rec
+                          (self (car tree))
+                          (if (cdr tree)
+                              (self (cdr tree)))))))
+    #'self))
+
+(defun trec (rec &optional (base #'identity))
+  (labels
+      ((self (tree)
+         (if (atom tree)
+             (if (functionp base)
+                 (funcall base tree)
+                 base)
+             (funcall rec tree
+                      #'(lambda ()
+                          (self (car tree)))
+                      #'(lambda ()
+                          (if (cdr tree)
+                              (self (cdr tree))))))))
+    #'self))
