032d2aa1faac31d0d837ef31ec599fb7a443d240
(Bit-Mage) <raj@thebitmage.com>
2024-11-15 16:56:39 +0530

    lisp
    Signed-off-by: (Bit-Mage) <raj@thebitmage.com>

--- lisp/.lisp
+++ lisp/.lisp
@@ -1,4 +1,4 @@
-(proclaim '(inline last1 single append1 nconc1 mklist))
+(proclaim '(inline last1 single append1 nconc1 mklist)) ;;; utilities
 
 (defun last1 (lst)
   (car (last lst)))
@@ -17,6 +17,7 @@
       obj
       (list obj)))
 
+
 (defun longer (x y)
   (labels ((compare (x y)
              (and (consp x)
@@ -27,7 +28,6 @@
              (listp y))
         (compare x y)
         (> (length x) (length y)))))
-
 
 (defun filter (fn lst)
   (let ((acc nil))
@@ -63,3 +63,125 @@
                                acc
                                (cons (car tree) acc)))))))
     (rec tree nil)))
+
+(defun findp (fn lst)
+  (if (null lst)
+      nil
+      (let ((val (funcall fn (car lst))))
+        (if val
+            (values (car lst) val)
+            (findp fn (cdr lst))))))
+
+(defun before (x y lst &key (test #'eql))
+  (cond ((not lst) nil)
+        ((funcall test (car lst) x) lst)
+        ((funcall test (car lst) y) nil)
+        (t (before x y (cdr lst) :test test))))
+
+(defun after (x y lst &key (test #'eql))
+  (let ((rest (before y x lst :test test)))
+    (and rest (member x rest :test test))))
+
+(defun duplicate (obj lst &key (test #'eql))
+  (member obj (cdr (member obj lst :test test))
+          :test test))
+
+
+(defun split-if (fn lst)
+  (let ((acc nil))
+    (do ((src lst (cdr src)))
+        ((or (null src) (funcall fn (car src)))
+         (values (nreverse acc) src))
+      (push (car src) acc))))
+
+(defun most (fn lst)
+  (if (null lst)
+      (values nil nil)
+      (let* ((wins (car lst))
+             (max (funcall fn wins)))
+        (dolist (obj (cdr lst))
+          (let ((score (funcall fn obj)))
+            (when (> score max)
+              (setq wins obj
+                    max score))))
+        (values wins max))))
+
+
+(defun best (fn lst)
+  (if (null lst)
+      nil
+      (let ((wins (car lst)))
+        (dolist (obj (cdr lst))
+          (if (funcall fn obj wins)
+              (setq wins obj)))
+        wins)))
+
+(defun mostn (fn lst)
+  (if (null lst)
+      (values nil nil)
+      (let ((result list (car lst))
+            (max (funcall fn (car lst))))
+        (dolist (obj (cdr lst))
+          (let ((score (funcall fn obj)))
+            (cond ((> score max)
+                   (setq max score
+                         result (list obj)))
+                  ((= score max)
+                   (push obj result)))))
+        (values (nreverse result) max))))
+
+
+(defun map-> (fn start test-fn succ-fn)
+  (do ((i start (funcall succ-fn i))
+       (result nil))
+      ((funcall test-fn i) (nreverse result))
+    (push (funcall fn i) result)))
+
+(defun mapa-b (fn a b &optional (step 1))
+  (map-> fn
+         a
+         (lambda (x) (> x b))
+         (lambda (x) (+ x step))))
+
+(defun map0-n (fn n)
+  (mapa-b fn 0 n))
+
+(defun map1-n (fn n)
+  (mapa-b fn 1 n))
+
+(defun mappend (fn &rest lsts)
+  (apply #'append (apply #'mapcar fn lsts)))
+
+(defun mapcars (fn &rest lsts)
+  (let ((result nil))
+    (dolist (lst lsts)
+      (dolist (obj lst)
+        (push (funcall fn obj) result)))
+    (nreverse result)))
+
+(defun rmapcar (fn &rest args)
+  (if (some #'atom args)
+      (apply fn args)
+      (apply #'mapcar
+             #'(lambda (&rest args)
+                 (apply #'rmapcar fn args))
+             args)))
+
+
+(defun readlist (&rest args)
+  (values (read-from-string
+           (concatenate 'string "("
+                        (apply #'read-line args)
+                        ")"))))
+
+(defun prompt (&rest args)
+  (apply #'format *query-io* args)
+  (read *query-io*))
+
+(defun break-loop (fn quit &rest args)
+  (format *query-io* "Entering break-loop. ~%")
+  (loop
+    (let ((in (apply #'prompt args)))
+      (if (funcall quit in)
+          (return)
+          (format *query-io* "~A~%" (funcall fn in))))))
