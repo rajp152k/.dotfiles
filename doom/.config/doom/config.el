7f66b7458968e8516800f4987b48736812f5f5e3
Raj Patil <rajp152k@gmail.com>
2025-03-13 11:42:15 +0530

    updates
    Signed-off-by: Raj Patil <rajp152k@gmail.com>

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,6 +1,5 @@
 ;;; Loading Secrets
 (load-file "/home/rp152k/.config/doom/utils.el")
-(load-file "/home/rp152k/.config/doom/gptel-conf.el")
 (load-file "/home/rp152k/.config/doom/secrets.el")
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
@@ -169,17 +168,28 @@
   (setq easy-hugo-basedir "/home/rp152k/source/vcops/thebitmage.com"))
 
                                         ;gptel
+
+(defvar GPTEL-PROVIDER "openai")
+(defvar GPTEL-MODELS (list
+                      (cons "openai"  'gpt-4o-mini)
+                      (cons "gemini"  'gemini-2.0-flash-exp)))
+(defvar GPTEL-PROMPTS
+  '(("base" .  "be precise, exhaustive, unbiased, analytical and critical")
+    ("epistemology" . "you are an intelligent epistemological engineer with factual expertise and insights that span across domains. You speak in logically discrete bullets first and presenting connections between the entities that you previously presented. when you feel that the current context is lacking, you ask specific questions regarding the details that would further help you reform your answer. finally, you provide insightful pathways in the form of questions in the end of your answer to direct further research on the topic of concern. You avoid using filler phrases and communicate with precision. You leverage formatting strategies like using tables for presenting differences, ascii diagrams to represent small flow charts. You do not use asterics and slashes in your output for any stylistic formatting (bold and italics) but just words unless explicitly asked to do so. You have a curious mindset that allows you to explore possibilities of cross domain applications. You are capable of thinking via multiple strategies and facets: via systems, in terms of abstractions, causality and other such cognitive constructs that help humans better present and structure information for personal and shared retrieval.")
+    ("swe" .  "you are an experienced sofware engineer that knows the ins and outs of sofware architecture, system design, how services scale and all the miscellaneous domain expertise that makes a succesful principal engineer that can conceptualize products from scratch.. You are also capable of dealing with complex abstractions and able to stitch novel solutions from all that you know. You act as an introspective colleague when dealing with quesions, thinking out loud for me and helping me understand the thought process of a curious, competent and ambitious engineer")))
+
 (use-package! gptel
   :config
   (setq
-   gptel-api-key (ascdr GPTEL-PROVIDER API-KEYS)
-   gptel-model  (ascdr GPTEL-PROVIDER GPTEL-MODELS)
+   gptel-api-key  (ascdr GPTEL-PROVIDER API-KEYS)
+   gptel-model   (ascdr GPTEL-PROVIDER GPTEL-MODELS))
    gptel-default-mode 'org-mode
    gptel--system-message (ascdr "base" GPTEL-PROMPTS)
    gptel-backend (eval `(,(symbol-function (intern (format "gptel-make-%s" GPTEL-PROVIDER)))
                          ,GPTEL-PROVIDER
                          :key ,gptel-api-key
-                         :stream t))))
+                         :models (list ',gptel-model)
+                         :stream t)))
 
 (defun gptel-prompt-alter ()
   "alter GPTEL prompt from a predefined list from gptel-conf.el "
